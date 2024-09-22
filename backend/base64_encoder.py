b117bd334c7a42b59f16d447502579590d6bf5e9
EKANSH SRIVASTAVA <116584951+ekansh1818@users.noreply.github.com>
2024-09-22 21:22:01 +0530

    push

--- backend/base64_encoder.py
+++
@@ -1,6 +0,0 @@
-import base64
-
-# Replace 'yourfile.txt' with the path to your file
-with open('yourfile.txt', 'rb') as file:
-    encoded_string = base64.b64encode(file.read())
-    print(encoded_string.decode('utf-8'))
